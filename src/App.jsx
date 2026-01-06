53fad5cd792174e8e32e57d4f0f0932d9c5c85ce
zkwo <ko40c980ew632@gmail.com>
2026-01-07 00:17:20 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import GetKey from './pages/GetKey';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-[#08080a] text-zinc-100 selection:bg-zinc-500/30">
+        <Navbar />
+        <main className="pt-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/getkey" element={<GetKey />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
